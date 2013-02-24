4a3315c5655ec581b44fb1bb267dab22400a7b03
Simon Tümmler <simon.tuemmler@rhs-bs.de>
2013-02-24 15:52:05 +0100

    Added libraries

--- MAVServer/src/is/hw/qdof/MAVServer/Controller.java
+++ MAVServer/src/is/hw/qdof/MAVServer/Controller.java
@@ -1,5 +1,22 @@
 package is.hw.qdof.MAVServer;
 
-public class Controller {
+import gnu.io.CommPortIdentifier;
 
+import java.util.Enumeration;
+
+public class Controller {
+	public static void main(String args[]) {
+		Enumeration<?> comPorts;
+		CommPortIdentifier currentIdent;
+		//
+		comPorts = CommPortIdentifier.getPortIdentifiers();
+		//
+		while (comPorts.hasMoreElements()) {
+			currentIdent = (CommPortIdentifier) comPorts.nextElement();
+			//
+			if(currentIdent.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+				System.out.println(currentIdent.getName());
+	    	}
+		}
+	}
 }
